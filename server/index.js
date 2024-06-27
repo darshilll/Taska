375b73b1fff4682559138f3439848d4b67012d1c
Darshil <darsgajul2003@gmail.com>
2024-06-27 17:40:29 +0530

    update: server

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", ""],
+    origin: ["https://task-manager-client-virid.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
