fd5f14f5c8d1596b1b53358ae4ad4c0183266f25
Darshil <darsgajul2003@gmail.com>
2024-06-30 23:27:11 +0530

    refactor: file changes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ import express from "express";
 import morgan from "morgan";
 import dbConnection from "./utils/index.js";
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewares.js";
-
 import routes from "./routers/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 dbConnection();
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__fileName);
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,10 +23,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://task-manager-client-virid.vercel.app",
-    ],
+    origin: ["http://localhost:8800"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -33,6 +34,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 app.use("/api", routes);
+
+//USE THE CLIENT APP
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+//RENDER CLIENT
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 
 app.use(routeNotFound);
 app.use(errorHandler);
