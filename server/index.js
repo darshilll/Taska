d8321064bdf4c54028a21aa78e264c82f145ecb9
Darshil <darsgajul2003@gmail.com>
2024-07-01 00:05:47 +0530

    refactor: apiSlice

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:8800"],
+    origin: ["https://task-manager-v8ej.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
