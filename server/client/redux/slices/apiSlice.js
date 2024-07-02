3c258e40f4d6cbff483c43360c9e69ab633a0714
Darshil <darsgajul2003@gmail.com>
2024-07-03 02:45:59 +0530

    update: link

--- server/client/redux/slices/apiSlice.js
+++ server/client/redux/slices/apiSlice.js
@@ -3,9 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = "http://localhost:8800";
 
-// const API_URL = "https://task-manager-wmys.onrender.com";
-
-const API_URL = import.meta.env.VITE_APP_BASE_URL;
+const API_URL = "https://task-manager-wmys.onrender.com";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL + "/api" });
 
