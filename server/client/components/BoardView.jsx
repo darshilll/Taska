f77867eb5578b0228896a1db319fcf96874ef1b4
Darshil Gajul <121224494+darshilll@users.noreply.github.com>
2025-02-28 00:46:17 +0530

    Update BoardView.jsx

--- server/client/components/BoardView.jsx
+++ server/client/components/BoardView.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/prop-types */
 import TaskCard from "./TaskCard";
 
-const BoardView = ({ tasks }) => {
+const BoardView = ({ tasks = [] }) => {
   return (
     <div className="w-full py-4">
-      {tasks.length === 0 ? (
+      {tasks?.length === 0 ? ( // Use optional chaining (?.) to prevent errors
         <div className="text-center text-gray-500 text-lg">
           No tasks are assigned
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 2xl:gap-10">
-          {tasks.map((task, index) => (
+          {tasks?.map((task, index) => (
             <TaskCard task={task} key={index} />
           ))}
         </div>
@@ -18,5 +18,6 @@ const BoardView = ({ tasks }) => {
     </div>
   );
 };
+
 
 export default BoardView;
